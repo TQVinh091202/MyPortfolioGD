949f40f0e03d605c6a9f3871dc95904ebac479a4
Fergus0912 <fergus@kongsoftware.com>
2024-08-12 21:54:25 +0700

    Portfolio GD

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var openPopup = document.getElementById('openPopup');
+    var closePopup = document.getElementById('closePopup');
+    var popup = document.getElementById('popup');
+
+    openPopup.addEventListener('click', function(event) {
+        event.preventDefault();
+        popup.style.display = 'flex';
+    });
+
+    closePopup.addEventListener('click', function(event) {
+        event.preventDefault();
+        popup.style.display = 'none';
+    });
+});
